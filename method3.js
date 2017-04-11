76858584b0f2b9fa99445f77c337f76faaffa1de
kylinpoet <kylinpoet@gmail.com>
2017-04-11 14:48:45 +0800

    Update method3.js

--- method3.js
+++ method3.js
@@ -1,31 +1,77 @@
-var direction;
-var angle;
-
-function do_something() {
-  direction = 0;
-  angle = 270;
-  while (true) {
-    if (angle < 40) {Q
-      stop();
-      swim(0);
-    }
-    if (angle > 40) {
-      swim(0);
-      cannon(angle, angle);
-    }
+function attack() {
+  for (var i = 0; i < 72; i++) {
+    if (scan(i * 5) <= 70) cannon(i * 5, scan(i * 5));
+    if (scan(i * 5 + 90) <= 70) cannon(i * 5 + 90, scan(i * 5 + 90));
+    if (scan(i * 5 + 180) <= 70) cannon(i * 5 + 180, scan(i * 5 + 180));
+    if (scan(i * 5 + 270) <= 70) cannon(i * 5 + 270, scan(i * 5 + 270));
   }
 }
-
-
-direction = 0;
-angle = 270;
-while (true) {
-  swim(angle);
-  while (scan(angle) > 50) {
-    angle = (typeof angle == 'number' ? angle : 0) + 5;
+while (health() >=75) {
+  while (loc_x() < 75) {
+    swim(0, 100);
   }
-  cannon(angle, scan(angle));
-  while (scan(direction) > 50) {
-    direction = (typeof direction == 'number' ? direction : 0) + -5;
+  stop();
+  if (Math.random() > 0.75) attack();
+  while (loc_y() > 25) {
+    swim(270, 100);
   }
+  stop();
+  if (Math.random() > 0.75) attack();
+  while (loc_x() > 25) {
+    swim(180, 100);
+  }
+  stop();
+  if (Math.random() > 0.75) attack();
+  while (loc_y() < 75) {
+    swim(90, 100);
+  }
+  stop();
+  if (Math.random() > 0.75) attack();
+}
+while (health() >= 50) {
+  while (loc_x() < 75) {
+    swim(0, 100);
+  }
+  stop();
+  if (Math.random() > 0.5) attack();
+  while (loc_y() > 25) {
+    swim(270, 100);
+  }
+  stop();
+  if (Math.random() > 0.5) attack();
+  while (loc_x() > 25) {
+    swim(180, 100);
+  }
+  stop();
+  if (Math.random() > 0.5) attack();
+  while (loc_y() < 75) {
+    swim(90, 100);
+  }
+  stop();
+  if (Math.random() > 0.5) attack();
+}
+while (health() >= 25) {
+  while (loc_x() < 75) {
+    swim(0, 100);
+  }
+  stop();
+  attack();
+  while (loc_y() > 25) {
+    swim(270, 100);
+  }
+  stop();
+  attack();
+  while (loc_x() > 25) {
+    swim(180, 100);
+  }
+  stop();
+  attack();
+  while (loc_y() < 75) {
+    swim(90, 100);
+  }
+  stop();
+  attack();
+}
+while (health() >= 0) {
+  attack();
 }
